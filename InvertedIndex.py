b6333b72f73dd0ba1ffecbcd283b49917af754cb
Ward Abbass <warda@wix.com>
2019-11-15 00:45:17 +0200

    add and or and not operators

--- InvertedIndex.py
+++ InvertedIndex.py
@@ -1,11 +1,26 @@
 import os
-import xml.etree.ElementTree as ElementTree
+from lxml import etree
 
 
 class Doc:
     def __init__(self, id, docNo):
         self.id = id
         self.docNo = docNo
+
+    def __lt__(self, other):
+        return other.id > self.id
+
+    def __le__(self, other):
+        return other.id >= self.id
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __gt__(self, other):
+        return self.id > other.id
+
+    def __ge__(self, other):
+        return self.id >= other.id
 
     def __repr__(self):
         return '(' + str(self.id) + ', ' + str(self.docNo) + ')'
@@ -36,14 +51,19 @@ def indexFile(filePath, invertedIndex, globalID):
     with open(filePath, 'r') as f:  # Reading file
         xml = f.read()
     xml = '<ROOT>' + xml + '</ROOT>'
-    root = ElementTree.fromstring(xml)
+    parser = etree.XMLParser(recover=True)
+    root = etree.fromstring(xml, parser=parser)
     for doc in root:
-        globalID += 1
-        docNo = doc.find("DOCNO").text.strip()
-        texts = doc.findall("TEXT")
-        doc = Doc(globalID, docNo)
-        indexTexts(doc, texts, invertedIndex)
-        # print(tokenizedText)
+        try:
+            docNo = doc.find("DOCNO").text.strip()
+            texts = doc.findall("TEXT")
+            if len(texts) > 0:
+                globalID += 1
+                doc = Doc(globalID, docNo)
+                indexTexts(doc, texts, invertedIndex)
+            # print(tokenizedText)
+        except:
+            print('errorrrrrr ', etree.tostring(doc))
     return globalID
 
 
@@ -59,15 +79,80 @@ def loadFiles():
 def InvertedIndex():
     invertedIndex = dict()
     files = loadFiles()
-    limit = 1
+    limit = 2000
     globalId = 0
     for file in files:
-        print(file)
         if limit > 0:
             limit -= 1
             globalId = indexFile(file, invertedIndex, globalId)
+            print(file)
         else:
             break
 
 
-InvertedIndex()
+def AND(left, right):
+    i, j = 0, 0
+    result = []
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            i += 1
+        elif right[j] < left[i]:
+            j += 1
+        else:
+            result.append(right[j])
+            j += 1
+            i += 1
+    return result
+
+
+def OR(left, right):
+    i, j = 0, 0
+    result = []
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        elif right[j] < left[i]:
+            result.append(right[j])
+            j += 1
+        else:
+            result.append(right[j])
+            j += 1
+            i += 1
+
+    # if left has no elements then merge the right elements
+    if j < len(right):
+        result.extend(right[j:])
+
+    # if right has no elements then merge the left elements
+    if i < len(left):
+        result.extend(left[i:])
+    return result
+
+
+def NOT(left, right):
+    i = 0
+    j = 0
+    result = []
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        elif right[j] < left[i]:
+            j += 1
+        else:
+            j += 1
+            i += 1
+    # if right has no elements then merge the left elements
+    if i < len(left):
+        result.extend(left[i:])
+    return result
+
+
+left = [Doc(1, ""), Doc(4, ""), Doc(5, "")]
+right = [Doc(0, ""), Doc(1, ""), Doc(2, ""), Doc(4, ""), Doc(7, "")]
+
+print(AND(left, right))
+print(OR(left, right))
+print(NOT(left, right))
